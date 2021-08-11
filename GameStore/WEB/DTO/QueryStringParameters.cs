83505134c2c593927b69fb5ff648eae9ae5dfcef
Sergei <sergeev.s1012@gmail.com>
2021-08-11 14:59:04 +0300

    change query parameters type

--- GameStore/WEB/DTO/QueryStringParameters.cs
+++ GameStore/WEB/DTO/QueryStringParameters.cs
@@ -5,8 +5,8 @@ namespace GameStore.WEB.DTO
 {
     public abstract class QueryStringParameters
     {
-        public uint Limit { get; set; } = FiltersConstants.PageSize;
-        public uint Offset { get; set; }
+        public int Limit { get; set; } = FiltersConstants.PageSize;
+        public int Offset { get; set; }
         public virtual string OrderBy { get; set; }
         public OrderType OrderType { get; set; }
     }
