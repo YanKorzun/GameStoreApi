c718c24892ed99f931f3b2c045a8c89a33c0de49
Sergei <sergeev.s1012@gmail.com>
2021-08-13 15:43:28 +0300

    begin unit tests

---
+++ GameStore.UnitTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using FakeItEasy;
+using GameStore.BL.Interfaces;
+using GameStore.WEB.Controllers;
+using Xunit;
+
+namespace GameStore.UnitTests.Controllers
+{
+    public class UserControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnApplicationUser()
+        {
+            //Arrange
+            var autoMapper = A.Fake<IMapper>();
+            var userService = A.Fake<IUserService>();
+
+            var contextUser =
+                new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new(ClaimTypes.NameIdentifier, "2") }));
+
+            var userController = new UserController(userService, autoMapper);
+            var result = await userController.GetInfo();
+
+            Assert.NotNull(result.Value);
+        }
+    }
+}
