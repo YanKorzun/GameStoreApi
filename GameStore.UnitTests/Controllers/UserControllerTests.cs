db8a62ae87d6d48034df219bd98ff4de4418574a
Sergei <sergeev.s1012@gmail.com>
2021-08-16 13:53:19 +0300

    delete ctor, add password patch test

--- GameStore.UnitTests/Controllers/UserControllerTests.cs
+++ GameStore.UnitTests/Controllers/UserControllerTests.cs
@@ -11,33 +11,34 @@ using GameStore.DAL.Entities;
 using GameStore.WEB.Controllers;
 using GameStore.WEB.DTO.Users;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace GameStore.UnitTests.Controllers
 {
     public class UserControllerTests
     {
-        private readonly ApplicationUser _appUser;
-        private readonly IMapper _autoMapper = A.Fake<IMapper>();
-
-        private readonly Fixture _fixture = new()
+        [Fact]
+        public async Task ShouldReturnApplicationUser()
         {
-            Behaviors = { new NullRecursionBehavior() }
-        };
+            //Arrange
+            var fixture = new Fixture
+            {
+                Behaviors = { new NullRecursionBehavior() }
+            };
 
-        private readonly UserController _userController;
+            var autoMapper = A.Fake<IMapper>();
+            var userService = A.Fake<IUserService>();
 
-        private readonly IUserService _userService = A.Fake<IUserService>();
-
-        public UserControllerTests()
-        {
-            _appUser = _fixture.Create<ApplicationUser>();
+            var appUser = fixture.Create<ApplicationUser>();
 
             var contextUser =
                 new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
                 {
-                    new(ClaimTypes.NameIdentifier, _appUser.Id.ToString())
+                    new(ClaimTypes.NameIdentifier, appUser.Id.ToString())
                 }));
 
 
@@ -49,44 +50,73 @@ namespace GameStore.UnitTests.Controllers
                 }
             };
 
-            _userController = new(_userService, _autoMapper)
+            var userController = new UserController(userService, autoMapper)
             {
                 ControllerContext = new()
                 {
                     HttpContext = httpContextAccessor.HttpContext
                 }
             };
-        }
 
-        [Fact]
-        public async Task ShouldReturnApplicationUser()
-        {
-            //Arrange
-            var searchResult = new ServiceResult<ApplicationUser>(ServiceResultType.Success, _appUser);
+            var searchResult = new ServiceResult<ApplicationUser>(ServiceResultType.Success, appUser);
 
-            A.CallTo(() => _userService.GetUserAsync(A<int>._)).Returns(searchResult);
+            A.CallTo(() => userService.GetUserAsync(A<int>._)).Returns(searchResult);
+
 
             //Act
-            var result = await _userController.GetInfo();
+            var result = await userController.GetInfo();
+
 
             //Assert
             var okResult = Assert.IsType<OkObjectResult>(result.Result);
             Assert.True(okResult.StatusCode.HasValue);
-            Assert.Equal(_appUser.Id, ((ApplicationUser)okResult.Value).Id);
-            Assert.Equal(_appUser.UserName, ((ApplicationUser)okResult.Value).UserName);
-            Assert.Equal(_appUser.Email, ((ApplicationUser)okResult.Value).Email);
-            Assert.Equal(_appUser.PhoneNumber, ((ApplicationUser)okResult.Value).PhoneNumber);
+            Assert.Equal(appUser.Id, ((ApplicationUser)okResult.Value).Id);
+            Assert.Equal(appUser.UserName, ((ApplicationUser)okResult.Value).UserName);
+            Assert.Equal(appUser.Email, ((ApplicationUser)okResult.Value).Email);
+            Assert.Equal(appUser.PhoneNumber, ((ApplicationUser)okResult.Value).PhoneNumber);
         }
 
 
         [Fact]
-        public async Task ShouldReturnUpdatedUser()
+        public async Task ShouldUpdateProfile()
         {
             //Arrange
-            _appUser.Id = _fixture.Create<int>();
+            var fixture = new Fixture
+            {
+                Behaviors = { new NullRecursionBehavior() }
+            };
 
-            var curUserDto = _fixture.Create<UpdateUserModel>();
-            var curUser = _fixture.Build<ApplicationUser>().With(o => o.Id, _appUser.Id).Create();
+            var autoMapper = A.Fake<IMapper>();
+            var userService = A.Fake<IUserService>();
+
+            var userId = fixture.Create<int>();
+
+            var contextUser =
+                new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new(ClaimTypes.NameIdentifier, userId.ToString())
+                }));
+
+
+            var httpContextAccessor = new HttpContextAccessor
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = contextUser
+                }
+            };
+
+            var userController = new UserController(userService, autoMapper)
+            {
+                ControllerContext = new()
+                {
+                    HttpContext = httpContextAccessor.HttpContext
+                }
+            };
+
+
+            var curUserDto = fixture.Create<UpdateUserModel>();
+            var curUser = fixture.Build<ApplicationUser>().With(o => o.Id, userId).Create();
 
             curUser.UserName = curUserDto.UserName;
             curUser.PhoneNumber = curUserDto.PhoneNumber;
@@ -94,11 +124,78 @@ namespace GameStore.UnitTests.Controllers
 
             var serviceResult = new ServiceResult<ApplicationUser>(ServiceResultType.Success, curUser);
 
-            A.CallTo(() => _userService.UpdateUserProfileAsync(A<int>._, A<UpdateUserModel>._)).Returns(serviceResult);
+            A.CallTo(() => userService.UpdateUserProfileAsync(A<int>._, A<UpdateUserModel>._)).Returns(serviceResult);
 
 
             //Act
-            var result = await _userController.UpdateUser(curUserDto);
+            var result = await userController.UpdateUser(curUserDto);
+
+
+            //Assert
+            Assert.IsType<NoContentResult>(result);
+        }
+
+
+        [Fact]
+        public async Task ShouldUpdatePassword()
+        {
+            //Arrange
+            var fixture = new Fixture
+            {
+                Behaviors = { new NullRecursionBehavior() }
+            };
+
+            var autoMapper = A.Fake<IMapper>();
+            var userService = A.Fake<IUserService>();
+
+            var patch = new JsonPatchDocument<BasicUserModel>
+            {
+                Operations = { new Operation<BasicUserModel>("replace", "/Password", null, "myNewPassword43@") }
+            };
+
+            var userId = fixture.Create<int>();
+
+            var contextUser =
+                new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new(ClaimTypes.NameIdentifier, userId.ToString())
+                }));
+
+
+            var httpContextAccessor = new HttpContextAccessor
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = contextUser
+                }
+            };
+
+            var userController = new UserController(userService, autoMapper)
+            {
+                ControllerContext = new()
+                {
+                    HttpContext = httpContextAccessor.HttpContext
+                }
+            };
+
+            var curUser = fixture.Build<ApplicationUser>().With(o => o.Id, userId).Create();
+            var patchedUser = JsonConvert.DeserializeObject<ApplicationUser>(JsonConvert.SerializeObject(curUser));
+
+            patchedUser.PasswordHash = fixture.Create<string>();
+
+            var getUserResult = new ServiceResult<ApplicationUser>(ServiceResultType.Success, curUser);
+            var getPatchedUserResult = new ServiceResult<ApplicationUser>(ServiceResultType.Success, patchedUser);
+
+            A.CallTo(() => userService.GetUserAsync(A<int>._))
+                .Returns(getUserResult);
+
+            A.CallTo(() => userService.UpdateUserPasswordAsync(A<ApplicationUser>._, A<BasicUserModel>._))
+                .Returns(getPatchedUserResult);
+
+
+            //Act
+            var result = await userController.UpdatePassword(patch);
+
 
             //Assert
             Assert.IsType<NoContentResult>(result);
