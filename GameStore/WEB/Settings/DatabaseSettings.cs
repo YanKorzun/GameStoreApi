d02129200811683e53a0073b4c44963398ba5064
SergeiHome <sergeev.s1012@gmail.com>
2021-08-24 10:57:02 +0300

    doesn't work

--- GameStore/WEB/Settings/DatabaseSettings.cs
+++ GameStore/WEB/Settings/DatabaseSettings.cs
@@ -4,7 +4,7 @@
     {
         public string Server { get; set; }
         public string Port { get; set; }
-        public string InitialCatalog { get; set; }
+        public string Database { get; set; }
         public string UserId { get; set; }
         public string Password { get; set; }
         public int ConnectTimeout { get; set; }
@@ -14,14 +14,15 @@
         public bool MultiSubnetFailover { get; set; }
 
         public string ConnectionString =>
-            $"Server={Server}, {Port};" +
-            $"Initial Catalog={InitialCatalog};" +
+            $"Server={Server},{Port};" +
+            $"Database={Database};" +
             $"User ID={UserId};" +
             $"Password={Password};" +
             $"Connect Timeout={ConnectTimeout};" +
             $"Encrypt={Encrypt};" +
             $"TrustServerCertificate={TrustedServerCertificate};" +
             $"ApplicationIntent={ApplicationIntent};" +
-            $"MultiSubnetFailover={MultiSubnetFailover}";
+            $"MultiSubnetFailover={MultiSubnetFailover};" +
+            $"MultipleActiveResultSets=True;";
     }
 }
